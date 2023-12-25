9f4a3565252b9822d9a0b52435150860fb4614f1
Manoj Kumar <154763376+kumarmanoj01@users.noreply.github.com>
2023-12-25 11:31:35 +0530

    source code
    This is a simple home page

---
+++ homepage.php
@@ -0,0 +1,119 @@
+<!Doctype html>
+<HTML>
+	<head>
+		<title>Home Page</title>
+		<link rel="stylesheet" type="text/css" href="tstyle.css">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=josefin+sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<!---hero Section Start ----->
+		
+			<nav>
+				<ul><table border="1px" cellspacing="0px">
+					<tr>
+					<td><li><a href="#"><i class="fa-solid fa-angle-left"></i></a></li></td>
+					</tr>
+				</table></ul><br>
+					
+				<h4 class="logo">Educare School <span>Admission</span> </h4>
+				<ul><table border="1px" cellspacing="0px"><tr>
+					<td><li><a href="#"><i class="fa-solid fa-laptop"></i></a></li></td>
+					<td><li><a href="#"><i class="fa-solid fa-desktop"></i></a></li></td>
+					<td><li><a href="#"><i class="fa-solid fa-tablet-screen-button"></i></a></li></td>
+					<td><li><a href="#"><i class="fa-solid fa-mobile-screen"></i></a></li></td>  
+					</td></tr></table>
+				</ul>
+				<a href="#" class="btn">For More</a>
+				<a href="#" class="btn">Subscribe</a>
+			</nav>
+			<div class="hero">
+			
+			</div>
+			<div class="content">
+				<h2>Admission</h2>
+				<p style="text-align: center;">We seek intellectually curious and motivated students with</p>
+				 <p style="text-align: center;">a record of achievement inside and outside the classroom. </p>
+				<p style="text-align: center;"> We select those who will benefit from our distinctive online </p>
+				<p style="text-align: center;"> curriculum and contribute to our community.</p>
+				
+				<div class="newslatter">
+					<form>
+						<input type="email" name="email" id="mail" placeholder="Enter Your Email">
+						<input type="submit" name="submit" value="Lets Start">
+					</form>
+				</div>
+			</div>
+
+			<div class="split box">
+				
+				
+					<table >
+						<tr><td><h3>YOUR APPLICATION BEGINS HERE</h3></td> </tr>
+
+						<tr><td>You have explored South Forest through this website and gotten a feel for who we are and how we operate, for our supportive staff, expert instructors, and global student community that enjoys a vibrant life inside and outside our virtual classrooms. You have decided that South Forest is a good fit as your full-time school, part-time school, or for a single course in a subject about which you are passionate.</td></tr>
+						<tr><td>Now, its time to begin your application by reading about the kinds of students we select, why we select them, and the information on which we base our admissions choices. Use the following checklist to ensure that you assemble and send us your information in a timely way for admission to the next acade year.</td></tr>
+				</table>
+				</div>
+				<div class=" split card">
+				<img src="school.jpg" alt="Paris" style="width:150px">
+				<h5>THE 2017-18 APPLICATION IS OPEN!</h5>
+				<h5>Already sure South Forest is the school for you?</h5>
+
+					<p style="text-align: center;">
+						<a class="button" href="pabout.php"> Read More</a>
+					</p>
+				</div>
+			</div>
+			</div>
+
+		
+			
+			<h1>ATTEND AN IN-PERSON OR VIRTUAL ADMISSIONS EVENT</h1>
+			<br>
+			<br>
+			<a href="#">School Fairs, Information Sessions, and Splash!</a> (In-Person)
+			<br>
+			<br>
+			<p>The Admissions Office welcomes the opportunity to connect with students at an in-person event.</p>
+			<br>
+			<br>
+			<a href="#">Sample Class</a> (Virtual)
+			<br>
+			<br>
+			<p>Theres no better way for you to see how it all works than to join a class. You all interact with students and an instructor, see how a virtual classroom functions, and learn more about our rigorous academics.</p>
+			<br>
+			<br>
+			<a href="#">Parent Coffee</a>(Virtual)
+			<br>
+			<br>
+			<p>You are likely to have questions about our school. Current Stanford OHS parents have the answers and love sharing their perspectives. Join them for an online "coffee" and ask them anything from how they manage socialization to what our academic curriculum has provided their child.</p>
+			<br>
+			<br>
+			<a href="#">Information Session (Virtual)</a>
+			<p>Want to learn more about what makes our school unique? Attend this live information session to hear from one of our admission staff.</p>
+		</div>
+
+		<!----footer Start ------->
+		<footer>
+			<img src="school.jpg" alt="Paris" style="width:150px">
+			<p style="color:white;"> 68 Hoang Dao Thuy Street, Hanoi, Vietnam</p>
+			<p style="color:white;">Information : 84 463 260 066</p>
+			<p style="color:white;">FAX : 84 463 260 088</p>
+			<a href="Joomlashine@bravebits.vn">Joomlashine@bravebits.vn</a>
+			<div class="social">
+				<a href="https://www.facebook.com/"><i class="fa-brands fa-facebook-f"></i> </a>
+				<a href="https://www.instagram.com/?hl=en"><i class="fa-brands fa-instagram-square"></i> </a>	
+				<a href="#"><i class="fa-brands fa-linkedin"></i></a>	
+				<a href="#"><i class="fa-brands fa-blogger-b"></i></a>
+			</div>	
+			<p style="color:white;">CopyRight By Manoj Kumar</p>
+		</footer>
+
+
+		</div>
+	</body>
+</HTML>
